d2b35c075a4a756458d9341119fde180d7c895cc
ProstoSensei <coder.tc@yandex.ru>
2023-11-03 00:14:49 +0300

    Реализован класс XMLtoJSON, конвертирующий файлы формата ".xml" в ".json"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,13 @@
+import java.io.IOException;
+
 public class Main
 {
+    public static void main(String[] args) throws IOException
+    {
+        //String jsonPath = "firstFile.json";
+        String xmlPath = "secondFile.xml";
 
+        XMLtoJSON.ConvertToJSON(xmlPath, "convertedFile.json");
+        //JSONtoXML.ConvertToXML(jsonPath, "");
+    }
 }
--- src/main/java/XMLtoJSON.java
+++ src/main/java/XMLtoJSON.java
@@ -1,2 +1,22 @@
-public class XMLtoJSON {
+import java.io.File;
+import java.io.IOException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+public class XMLtoJSON
+{
+    public static void ConvertToJSON(String filePath, String outputPath) throws IOException
+    {
+        File xmlFile = new File(filePath);
+        File jsonFile = new File(outputPath);
+        ObjectMapper xmlMapper = new XmlMapper();
+        Object xmlObject = xmlMapper.readValue(xmlFile, Object.class);
+        ObjectMapper jsonMapper = new ObjectMapper();
+        jsonMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        jsonMapper.writeValue(jsonFile, xmlObject);
+
+        System.out.println(jsonMapper.writeValueAsString(xmlObject));
+    }
 }
